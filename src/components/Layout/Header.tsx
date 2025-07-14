14cddfef5298b8baa701a3101b1de6a7e471d49a
Muhammad Nasir Rasheed <142977146+nasirrasheed@users.noreply.github.com>
2025-07-14 12:27:49 -0700

    Update Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-12 left-0 right-0 bg-black/90 backdrop-blur-md z-40 border-b border-white/10">
+    <header className=" top-12 left-0 right-0 bg-black/90 backdrop-blur-md z-40 border-b border-white/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           {/* Logo */}
